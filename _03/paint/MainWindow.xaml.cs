b36c66350298845af7d4a05b228a7abd2c61ce05
markiiza <113509038+markiiza@users.noreply.github.com>
2022-12-21 23:27:48 +0500

    WPF_INTERFASE

---
+++ _03/paint/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace paint
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void valuechanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (null != slid && Pen != null)
+            {
+                Pen.Width = slid.Value;
+                Pen.Height = slid.Value;
+            }             
+        }
+
+        private void select(object sender, SelectionChangedEventArgs e)
+        {
+            if (null != slid && Pen != null)
+            {
+                if (selectBox.SelectedIndex == 0)
+                {
+                    Pen.Color = Colors.Red;
+                }
+                if (selectBox.SelectedIndex == 1)
+                {
+                    Pen.Color = Colors.Orange;
+                }
+                if (selectBox.SelectedIndex == 2)
+                {
+                    Pen.Color = Colors.Yellow;
+                }
+                if (selectBox.SelectedIndex == 3)
+                {
+                    Pen.Color = Colors.Green;
+                }
+                if (selectBox.SelectedIndex == 4)
+                {
+                    Pen.Color = Colors.Aqua;
+                }
+                if (selectBox.SelectedIndex == 5)
+                {
+                    Pen.Color = Colors.Blue;
+                }
+                if (selectBox.SelectedIndex == 6)
+                {
+                    Pen.Color = Colors.BlueViolet;
+                }
+            }
+        }
+
+        private void Changed(object sender, RoutedEventArgs e)
+        {
+            if (RadioButton_Boot1.IsChecked == true)
+                holst.EditingMode = InkCanvasEditingMode.Ink;
+            else
+            if (RadioButton_Boot2.IsChecked == true)
+                holst.EditingMode = InkCanvasEditingMode.Select;
+            else
+            if (RadioButton_Boot3.IsChecked == true)
+                holst.EditingMode = InkCanvasEditingMode.EraseByStroke;
+
+        }
+    }
+}
---
+++ _04/catsP/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zadanoeone
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void red(object sender, RoutedEventArgs e)
+        {
+            gridiiii.Background = new SolidColorBrush(Colors.Red);
+        }
+        private void yellow(object sender, RoutedEventArgs e)
+        {
+            gridiiii.Background = new SolidColorBrush(Colors.Yellow);
+        }
+        private void green(object sender, RoutedEventArgs e)
+        {
+            gridiiii.Background = new SolidColorBrush(Colors.Green);
+        }
+        private void lightblue(object sender, RoutedEventArgs e)
+        {
+            gridiiii.Background = new SolidColorBrush(Colors.DeepSkyBlue);
+        }
+        private void blue(object sender, RoutedEventArgs e)
+        {
+            gridiiii.Background = new SolidColorBrush(Colors.Blue);
+        }
+        private void fiolet(object sender, RoutedEventArgs e)
+        {
+            gridiiii.Background = new SolidColorBrush(Colors.Violet);
+        }
+
+        private void rose(object sender, RoutedEventArgs e)
+        {
+            gridiiii.Background = new SolidColorBrush(Colors.LightPink);
+        }
+
+        private void orange(object sender, RoutedEventArgs e)
+        {
+            gridiiii.Background = new SolidColorBrush(Colors.Orange);
+        }
+        private void mess(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Разработчик: ученица группы ПР-31 Седунова Елизавета");
+        }
+        private void clo(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void mousemove(object sender, MouseEventArgs e)
+        {
+            one.Text = " ";
+            two.Text = "Информация о разработчике";
+           
+        }
+
+        private void MouseEnter(object sender, MouseEventArgs e)
+        {
+            one.Text = " ";
+            two.Text = "Выход";
+        }
+
+        private void Image1_MouseEnter(object sender, MouseEventArgs e)
+        {
+            one.Text = "Изменить фон на: ";
+            two.Text = "Красный";
+        }
+        private void Image2_MouseEnter(object sender, MouseEventArgs e)
+        {
+            one.Text = "Изменить фон на: ";
+            two.Text = "Оранжевый";
+        }
+        private void Image3_MouseEnter(object sender, MouseEventArgs e)
+        {
+            one.Text = "Изменить фон на: ";
+            two.Text = "Жёлтый";
+        }
+        private void Image4_MouseEnter(object sender, MouseEventArgs e)
+        {
+            one.Text = "Изменить фон на: ";
+            two.Text = "Голубой";
+        }
+        private void Image5_MouseEnter(object sender, MouseEventArgs e)
+        {
+            one.Text = "Изменить фон на: ";
+            two.Text = "Розовый";
+        }
+    }
+}
